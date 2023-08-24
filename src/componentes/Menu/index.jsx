d77fdc2efa74f61edd9551b9b93066d0b7367bac
pedroquintao <pedro.h.quintao@hotmail.com>
2023-08-24 20:17:21 -0300

    Versão final do projeto do curso

--- src/componentes/Menu/index.jsx
+++ src/componentes/Menu/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import home from '../../assets/icones/home.png'
-import maisCurtidas from '../../assets/icones/mais-curtidas.png'
-import maisVistas from '../../assets/icones/mais-vistas.png'
-import novas from '../../assets/icones/novas.png'
-import surpreendaMe from '../../assets/icones/surpreenda-me.png'
+import home from 'assets/icones/home.png'
+import maisCurtidas from 'assets/icones/mais-curtidas.png'
+import maisVistas from 'assets/icones/mais-vistas.png'
+import novas from 'assets/icones/novas.png'
+import surpreendaMe from 'assets/icones/surpreenda-me.png'
 import styles from './Menu.module.scss';
 
 export default function Menu() {
